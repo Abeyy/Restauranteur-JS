1ef24386c38ca6f10104654cfda5b457416c232a
Abeyy <abey.joy@nyu.edu>
2018-06-25 00:31:58 -0400

    add some js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function calcTip() {
   let totalAmount   = document.getElementById('totalAmount').value;
   let tipPercentage = document.getElementById('tipPercentage').value;
-  let tip = tipPercentage / 100
-  console.log(totalAmount * tip)
+  let tip =  totalAmount * (tipPercentage / 100);
+  document.getElementById('results').innerHTML = tip;
 }
