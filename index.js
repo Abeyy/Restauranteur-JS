92d4eb1c79e294fddece82865d4e41098eb48138
Abeyy <abey.joy@nyu.edu>
2018-06-28 23:48:29 -0400

    Delete everything, make it a vue app soon

--- index.js
+++
@@ -1,6 +0,0 @@
-function calcTip() {
-  let totalAmount   = document.getElementById('totalAmount').value;
-  let tipPercentage = document.getElementById('tipPercentage').value;
-  let tip =  totalAmount * (tipPercentage / 100);
-  document.getElementById('results').innerHTML = tip;
-}
