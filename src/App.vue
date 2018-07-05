dc29ac1681def03af318a5255589fe0814fd65f6
Abeyy <abey.joy@nyu.edu>
2018-07-05 01:24:21 -0400

    Add second component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <EnterUsers v-if="currentView == 'enterUsers'"></EnterUsers>
+  <div class="app">
+    <EnterUsers v-if="currentView == 'enterUsers'"></EnterUsers>
+    <OrderedItems v-if="currentView == 'orderedItems'"></OrderedItems>
+  </div>
 </template>
 
 <script>
 
 import EnterUsers from './components/EnterUsers.vue'
+import OrderedItems from './components/OrderedItems.vue'
+
 export default {
   components: {
-    EnterUsers: EnterUsers
+    EnterUsers: EnterUsers,
+    OrderedItems: OrderedItems
   },
   computed: {
     currentView() {
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     userList: [],
-    views: ['enterUsers', 'enterOrders'],
+    views: ['enterUsers', 'orderedItems'],
     currentView: 'enterUsers'
   },
   mutations: {
